b100c9d14739e6cf2457ec7159dcc54f2d7f749b
kato83 <katomeganeb83@gmail.com>
2024-11-18 02:43:36 +0000

    TODOアプリケーション作成

--- README.md
+++ README.md
@@ -7,3 +7,17 @@ $ npm install
 $ npm run dev
 # 表示されるコンソール上のURLよりブラウザ上でプレビューすることができます
 ```
+
+
+```html
+      <div class="todo-card">
+        <input type="checkbox" />
+        <span class="todo-text" data-id="96004b06-12c8-46fc-9ba5-3b4feee2cff8" contenteditable="true">未完了のTODO 01</span>
+        <button class="delete-btn">削除</button>
+      </div>
+      <div class="todo-card todo-completed">
+        <input type="checkbox" checked>
+        <span class="todo-text" data-id="6f989167-4adf-4476-b4fc-95b0714c03c7" contenteditable="true">完了のTODO 04</span>
+        <button class="delete-btn">削除</button>
+      </div>
+```
--- main.js
+++ main.js
@@ -5,3 +5,98 @@ const getTodos = () => {
 
 [...document.getElementsByClassName('delete-btn')]
   .forEach(node => node.addEventListener('click', () => console.log(confirm('このTODOを削除してもよろしいですか？'))));
+
+/**
+ * HTMLエスケープを実施する関数
+ * @param {string} str エスケープしたい対象の文字列
+ * @returns エスケープした結果の文字列
+ */
+const escapeHTML = (str) => str.replace(/[&'`"<>]/g, match => ({
+  '&': '&amp;',
+  "'": '&#x27;',
+  '`': '&#x60;',
+  '"': '&quot;',
+  '<': '&lt;',
+  '>': '&gt;',
+}[match]));
+
+/**
+ * 画面の描画処理
+ */
+const render = () => {
+  /** @type {Array<{completed: boolean, text: string, id: string}>} */
+  const todos = getTodos();
+  const completedTodoList = document.querySelector('#completed-todo-list');
+  completedTodoList.innerHTML = '';
+  const uncompletedTodoList = document.querySelector('#uncompleted-todo-list');
+  uncompletedTodoList.innerHTML = '';
+  todos.forEach(todo => {
+    const todoHTML = `<div class="todo-card ${todo.completed ? 'todo-completed' : ''}" data-id="${todo.id}">
+        <input type="checkbox" ${todo.completed ? 'checked' : ''} />
+        <span class="todo-text" contenteditable="true">${escapeHTML(todo.text)}</span>
+        <button class="delete-btn">削除</button>
+      </div>`;
+    if (todo.completed) {
+      document.querySelector('#completed-todo-list').insertAdjacentHTML('beforeend', todoHTML);
+    } else {
+      document.querySelector('#uncompleted-todo-list').insertAdjacentHTML('beforeend', todoHTML);
+    }
+  });
+};
+
+// form submit 時の処理
+document.querySelector('#todo-form')
+  .addEventListener('submit', (e) => {
+    // ページリロードを防ぐ
+    e.preventDefault();
+    // localStorage の保存処理
+    const form = new FormData(e.target);
+    const text = form.get('text');
+    const todos = [
+      ...getTodos(),
+      {
+        completed: false,
+        text: text,
+        id: crypto.randomUUID()
+      }
+    ];
+    localStorage.setItem('todos', JSON.stringify(todos));
+    render();
+    // form の内容をリセットする
+    e.target.reset();
+  });
+
+render();
+
+// 削除処理
+document.addEventListener('click', (e) => {
+  if (e.target.classList.contains('delete-btn') && confirm('このTODOを削除してもよろしいですか？')) {
+    /** @type {HTMLElement} */
+    const todoElement = e.target.closest('.todo-card');
+    const uuid = todoElement.dataset.id;
+    /** @type {Array<{completed: boolean, text: string, id: string}>} */
+    const todos = getTodos();
+    const newTodos = todos.filter(todo => todo.id !== uuid);
+    localStorage.setItem('todos', JSON.stringify(newTodos));
+    render();
+  }
+});
+
+// チェックボックスによる完了、未完了をセットするようにする
+document.addEventListener('change', (e) => {
+  if (e.target.type === 'checkbox') {
+    /** @type {HTMLElement} */
+    const todoElement = e.target.closest('.todo-card');
+    const uuid = todoElement.dataset.id;
+    /** @type {Array<{completed: boolean, text: string, id: string}>} */
+    const todos = getTodos();
+    for (const todo of todos) {
+      // チェックボックスを変更した対象のTODOかチェック
+      if (todo.id === uuid) {
+        todo.completed = !todo.completed;
+      }
+    }
+    localStorage.setItem('todos', JSON.stringify(todos));
+    render();
+  }
+});
